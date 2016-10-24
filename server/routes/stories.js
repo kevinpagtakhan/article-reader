a144b24986c91b10bf07b0a178fc56334d390d9d
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-10-24 09:14:05 -0700

    Save api response to mongodb

--- server/routes/stories.js
+++ server/routes/stories.js
@@ -21,12 +21,30 @@ router.route('/')
       "endpoint": ["summarize", "extract"]
     }, function(err, result) {
       if (err === null) {
-        result.results.forEach(function(r) {
-          console.log(r.endpoint + ':');
-          console.log(r.result);
-        });
+
+        // result.results.forEach(function(r) {
+        //   console.log(r.endpoint + ':');
+        //   console.log(r.result);
+        // });
+
+        var article = result.results;
+        var newStory = {
+          url: req.body.url,
+          title: article[0].result.title,
+          author: article[0].result.author,
+          text: article[0].result.article,
+          image: article[0].result.image,
+          publishDate: article[0].result.publishDate,
+          sentences: article[1].result.sentences
+        }
+
+        Story.create(newStory, function(err, data){
+          res.json(err || data);
+        })
       } else {
-        console.log(err)
+        // console.log(err)
+
+        res.json(err);
       }
     });
   })
